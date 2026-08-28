eb2695c455ee4a040f77d34c33452794a0df3b65
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-08-28 16:54:47 +0100

    Create page.vue

---
+++ page.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="markdown-body container-lg px-3 my-5">
+    <h1><a href="https://web4application.github.io/kubu-hai.model.h5/">kubu-hai.model.h5</a></h1>
+    <h1 id="migration">Migration</h1>
+
+    <ul>
+      <li><a href="#multiple-uploads-to-the-same-named-artifact">Multiple uploads to the same named Artifact</a></li>
+      <li><a href="#overwriting-an-artifact">Overwriting an Artifact</a></li>
+      <li><a href="#merging-multiple-artifacts">Merging multiple artifacts</a></li>
+      <li><a href="#working-with-immutable-artifacts">Working with Immutable Artifacts</a></li>
+    </ul>
+
+    <p>Several behavioral differences exist between Artifact actions <code>v3</code> and <code>v4</code>. This document outlines how common scenarios differ.</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+// No logic needed yet
+</script>
+
+<style scoped>
+@import '@/assets/style.css';
+</style>
